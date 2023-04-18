b4c1e81c7d8982fd46141e0498775c0378b6dbe0
SX-7 <sn1.test.preria.2002@gmail.com>
2023-04-18 12:28:33 +0200

    Added basic discord functionality

--- main.py
+++ main.py
@@ -3,26 +3,47 @@ from kafka.errors import KafkaError
 import msgpack
 from dotenv import load_dotenv
 import os
+import discord
 
 load_dotenv()
 
 server_ip = os.getenv("SERVER_IP")
+bot_token=os.getenv("DISCORD_BOT_TOKEN")
 
 producer = KafkaProducer(bootstrap_servers=[str(server_ip+':9092')],
                          retries=5,
-                         value_serializer=msgpack.dumps)
+                         value_serializer=msgpack.dumps,
+                         key_serializer=msgpack.dumps)
 
 print(producer.bootstrap_connected())
-# Asynchronous by default
-future = producer.send(topic='my-topic', value='value')
 
-# Block for 'synchronous' sends
-try:
-    record_metadata = future.get(timeout=10)
-    print("%s:%d:%d"%(record_metadata.topic,record_metadata.partition,record_metadata.offset))
-except KafkaError as err:
-    # Decide what to do if produce request failed...
-    print(err.args)
-    pass
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Asynchronous by default
+    future = producer.send(topic='my-topic', value=message.content, key=message.author.name+"#"+message.author.discriminator)
+
+    # Block for 'synchronous' sends
+    try:
+        record_metadata = future.get(timeout=10)
+        print("%s:%d:%d" % (record_metadata.topic,
+          record_metadata.partition, record_metadata.offset))
+    except KafkaError as err:
+        # Decide what to do if produce request failed...
+        print(err.args)
+        pass
+
+
+client.run(bot_token)
 
 producer.flush()
