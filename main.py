560f5562432f9c1ac23bcbcb2e4d212b979577d5
SX-7 <sn1.test.preria.2002@gmail.com>
2023-04-30 00:03:01 +0200

    Added typing. Standardized formats

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
+# General idea:
+# - Estabilish connectivity between two servers
+# - Convert+send messages over to Kafka server
+# In order to limit the boilerplate on producer side, decrease data size, we're only sending basic info, and standardazing it whenever we can
+# It is up to consumer to create meaningful messages in UI from data from Kafka server
+
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import msgpack
 from dotenv import load_dotenv
 import os
 import discord
+from typing import Union, Any
 
+# load data
 load_dotenv()
 
 server_ip = os.getenv("SERVER_IP")
 bot_token = os.getenv("DISCORD_BOT_TOKEN")
-
-producer = KafkaProducer(bootstrap_servers=[str(server_ip+':9092')],
+# init connection
+producer = KafkaProducer(bootstrap_servers=[str(server_ip)],
                          retries=5,
                          value_serializer=msgpack.dumps,
                          key_serializer=msgpack.dumps)
 
-print(producer.bootstrap_connected())
+if producer.bootstrap_connected():
+    print("Estabilished connection with Kafka server")
 
-intents = discord.Intents.all()
+# default logging config for discord.py will print info on success
+client = discord.Client(intents=discord.Intents.all())
 
-client = discord.Client(intents=intents)
+
+# TODO: More asynchronicity
 
 
-def send_message(producer, message_key, message_data):
+def send_message(producer: KafkaProducer, message_key: str, message_data: dict[str, Any]) -> None:
+    # helper function for sending data
     future = producer.send(topic='my-topic', value=message_data,
                            key=message_key)
     # Block for 'synchronous' sends
@@ -36,205 +48,132 @@ def send_message(producer, message_key, message_data):
         pass
 
 
+def extract_message_data(message: discord.Message) -> dict[str, Any]:
+    return {
+        "id": message.id,
+        "author": {"name": str(message.author), "id": message.author.id},
+        "content": message.clean_content if message.type is discord.MessageType.default or message.type is discord.MessageType.reply else message.system_content,
+        "channel": {"name": message.channel.name, "id": message.channel.id},
+        "created": message.created_at.isoformat(' ', timespec='seconds'),
+        "edited": message.edited_at.isoformat(' ', timespec='seconds') if message.edited_at is not None else None,
+        "url": message.jump_url,
+        "type": message.type,
+        "attachments": [{"name": attachment.filename, "url": attachment.url} for attachment in message.attachments],
+        "stickers": [{"name": sticker.name, "url": sticker.url}for sticker in message.stickers],
+        "reference": {"id": message.reference.message_id, "url": message.reference.jump_url} if message.reference is not None else None
+    }
+
+
+def extract_reaction_data(reaction: discord.Reaction) -> dict[str, Any]:
+    return {
+        "name": reaction.emoji if isinstance(reaction.emoji, str) else reaction.emoji.name,
+        "count": reaction.count,
+        "url":  None if isinstance(reaction.emoji, str) else reaction.emoji.url
+    }
+
+
 @client.event
+# useful paste-in from documentiation
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+# TODO: Change to raws? To ensure catching
+
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
+        # safety feature, though if it ever gets used, you likely messed up the bot
         return
-
     message_data = {
-        "action": "Message sent",
         "action_id": 1,
-        "author": message.author.name+"#"+message.author.discriminator,
-        "author_id": message.author.id,
-        "content": message.system_content,
-        "channel": message.channel.name,
-        "created": message.created_at.isoformat(' ', timespec='seconds'),
-        "url": message.jump_url,
-        "type": message.type,
-        "embeds": [embed.to_dict() for embed in message.embeds],
-        "attachments": [attachment.url for attachment in message.attachments]
+        "message": extract_message_data(message=message)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=message.guild.id)
 
-# TODO: Change to raws? To ensure catching
-# TODO: passing keys?
-# TODO: Make messages passed more compact
+
 # TODO: increase consistency between messages
 
+
 @client.event
-async def on_message_edit(before, after):
-    # action is consistent, we can have the same id too, author, channel, url is the same too?
-    before_data = {
-        "content": before.system_content,
-        "created": before.created_at.isoformat(' ', timespec='seconds'),
-        "type": before.type,
-        "embeds": [embed.to_dict() for embed in before.embeds],
-        "attachments": [attachment.url for attachment in before.attachments]
-    }
-    after_data = {
-        "content": after.system_content,
-        "edited": after.edited_at.isoformat(' ', timespec='seconds'),
-        "type": after.type,
-        "embeds": [embed.to_dict() for embed in after.embeds],
-        "attachments": [attachment.url for attachment in after.attachments]
-    }
+async def on_message_edit(before: discord.Message, after: discord.Message):
+    if after.edited_at is None:
+        # sad neccesity due to weird discord embed handling
+        return
     message_data = {
-        "action": "Message edited",
         "action_id": 2,
-        "author": after.author.name+"#"+after.author.discriminator,
-        "author_id": after.author.id,
-        "channel": after.channel.name,
-        "url": after.jump_url,
-        "before": before_data,
-        "after": after_data
+        "before": extract_message_data(before),
+        "after": extract_message_data(after)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=before.guild.id)
 
 
 @client.event
-async def on_message_delete(message):
+async def on_message_delete(message: discord.Message):
     message_data = {
-        "action": "Message deleted",
         "action_id": 3,
-        "author": message.author.name+"#"+message.author.discriminator,
-        "author_id": message.author.id,
-        "content": message.system_content,
-        "channel": message.channel.name,
-        "created": message.created_at.isoformat(' ', timespec='seconds'),
-        "url": message.jump_url,
-        "type": message.type,
-        "embeds": [embed.to_dict() for embed in message.embeds],
-        "attachments": [attachment.url for attachment in message.attachments]
+        "message": extract_message_data(message)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=message.guild.id)
 
 
 @client.event
-async def on_bulk_message_delete(messages):
+async def on_bulk_message_delete(messages: list[discord.Message]):
     message_data = {
-        "action": "Messages bulk deleted",
         "action_id": 4,
-        "messages": [
-            {
-                "author": message.author.name+"#"+message.author.discriminator,
-                "author_id": message.author.id,
-                "content": message.system_content,
-                "channel": message.channel.name,
-                "created": message.created_at.isoformat(' ', timespec='seconds'),
-                "url": message.jump_url,
-                "type": message.type,
-                "embeds": [embed.to_dict() for embed in message.embeds],
-                "attachments": [attachment.url for attachment in message.attachments]
-            } for message in messages
-        ]
+        "messages": [extract_message_data(message)for message in messages]
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=messages[0].guild.id)
 
 
 @client.event
-async def on_reaction_add(reaction, user):
+async def on_reaction_add(reaction: discord.Reaction, user: Union[discord.Member, discord.User]):
     message_data = {
-        "action": "Reaction added",
         "action_id": 5,
-        "reactee": user.name+"#"+user.discriminator,
-        "message": {
-            # TODO trim it down
-            "action": "Message sent",
-            "action_id": 1,
-            "author": reaction.message.author.name+"#"+reaction.message.author.discriminator,
-            "author_id": reaction.message.author.id,
-            "content": reaction.message.system_content,
-            "channel": reaction.message.channel.name,
-            "created": reaction.message.created_at.isoformat(' ', timespec='seconds'),
-            "url": reaction.message.jump_url,
-            "type": reaction.message.type,
-            "embeds": [embed.to_dict() for embed in reaction.message.embeds],
-            "attachments": [attachment.url for attachment in reaction.message.attachments]
-        },
-        "reaction": reaction.emoji.name
+        "user": str(user),
+        "message": extract_message_data(reaction.message),
+        "reaction": extract_reaction_data(reaction)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=reaction.message.guild.id)
 
 
 @client.event
-async def on_reaction_remove(reaction, user):
+async def on_reaction_remove(reaction: discord.Reaction, user: Union[discord.Member, discord.User]):
     message_data = {
-        "action": "Reaction removed",
         "action_id": 6,
-        "reactee": user.name+"#"+user.discriminator,
-        "message": {
-            # TODO trim it down
-            "action": "Message sent",
-            "action_id": 1,
-            "author": reaction.message.author.name+"#"+reaction.message.author.discriminator,
-            "author_id": reaction.message.author.id,
-            "content": reaction.message.system_content,
-            "channel": reaction.message.channel.name,
-            "created": reaction.message.created_at.isoformat(' ', timespec='seconds'),
-            "url": reaction.message.jump_url,
-            "type": reaction.message.type,
-            "embeds": [embed.to_dict() for embed in reaction.message.embeds],
-            "attachments": [attachment.url for attachment in reaction.message.attachments]
-        },
-        "reaction": reaction.emoji.name
+        "user": str(user),
+        "message": extract_message_data(reaction.message),
+        "reaction": extract_reaction_data(reaction)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=reaction.message.guild.id)
 
 
 @client.event
-async def on_reaction_clear(message, reactions):
+async def on_reaction_clear(message: discord.Message, reactions: list[discord.Reaction]):
     message_data = {
-        "action": "Reaction bulk cleared",
         "action_id": 7,
-        "message": {
-            # TODO trim it down
-            "action": "Message sent",
-            "action_id": 1,
-            "author": message.author.name+"#"+message.author.discriminator,
-            "author_id": message.author.id,
-            "content": message.system_content,
-            "channel": message.channel.name,
-            "created": message.created_at.isoformat(' ', timespec='seconds'),
-            "url": message.jump_url,
-            "type": message.type,
-            "embeds": [embed.to_dict() for embed in message.embeds],
-            "attachments": [attachment.url for attachment in message.attachments]
-        },
-        "reaction": [[reaction.emoji.name, reaction.emoji.count]for reaction in reactions]
+        "message": extract_message_data(message),
+        "reaction": [{extract_reaction_data(reaction)}for reaction in reactions]
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=message.guild.id)
 
 
 @client.event
-async def on_reaction_clear_emoji(reaction):
+async def on_reaction_clear_emoji(reaction: discord.Reaction):
     message_data = {
-        "action": "Reaction fullly cleared",
         "action_id": 8,
-        "message": {
-            # TODO trim it down
-            "action": "Message sent",
-            "action_id": 1,
-            "author": reaction.message.author.name+"#"+reaction.message.author.discriminator,
-            "author_id": reaction.message.author.id,
-            "content": reaction.message.system_content,
-            "channel": reaction.message.channel.name,
-            "created": reaction.message.created_at.isoformat(' ', timespec='seconds'),
-            "url": reaction.message.jump_url,
-            "type": reaction.message.type,
-            "embeds": [embed.to_dict() for embed in reaction.message.embeds],
-            "attachments": [attachment.url for attachment in reaction.message.attachments]
-        },
-        "reaction": {
-            "name":reaction.emoji.name, 
-            "count":reaction.emoji.count
-        }
+        "message": extract_message_data(reaction.message),
+        "reaction": extract_reaction_data(reaction)
     }
-    send_message(producer=producer, message_data=message_data)
+    send_message(producer=producer, message_data=message_data,
+                 message_key=reaction.message.guild.id)
 
 client.run(bot_token)
 
